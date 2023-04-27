274af2d0ecc134253092f0cee5649547e4a496ef
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-04-27 23:25:33 +0300

    add lightbox

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 // import { fetchPictures } from "./fetchPictures";
-// import axios from "axios";
+import axios from "axios";
 import {Notify} from "notiflix";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+let lightbox = new SimpleLightbox('.gallery a');
 
 const API_KEY = "29210870-5c756012ce316252fd55732c8";
 const BASE_URL = "https://pixabay.com/api/";
 let page = 1;
-
 let getGallery;
-// let galleryData = [];
 let images = [];
 
 
@@ -25,8 +27,14 @@ refs.form.addEventListener("submit", onSearchBtn);
 refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
 function fetchPictures(name) {
+
+  if(getGallery === "") {
+    errorMessage()
+    return;
+  }
+  
   return fetch(`${BASE_URL}?key=${API_KEY}&q=${name}&image-type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`)
-  .then( response => {
+  .then(response => {
       if(!response.ok) {
           throw new Error()
       }
@@ -34,20 +42,18 @@ function fetchPictures(name) {
   })
   .then(data => {
       console.log('data',data);
+      refs.loadMoreBtn.style.display = "block";
       return data;
   })
-
 }
 
 function onLoadMoreBtnClick(e) {
   e.preventDefault();
   page += 1;
   fetchPictures()
-  .then(data => {
-    console.log('data', data);
-    images.push(data.hits)
-    createGalleryMarkup(images)
- 
+  .then(newData => {
+    console.log('newData', newData);
+    createGalleryMarkup(newData.hits)
   })
 }
 
@@ -66,20 +72,13 @@ function onSearchBtn(e) {
 }
 
 function createGalleryMarkup(images) {
-
-  if(getGallery === "") {
-    Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-    return;
-  }
-    refs.loadMoreBtn.style.display = "block";
-
     const markupGallery = images.map(({webformatURL,largeImageURL,tags,likes,views,comments,downloads}) => {
       return `
           <div class="photo-card">
             <div class="img-thumb">
                 <a class="gallery_link" href="${webformatURL}">
                 <img  class="gallery__image" 
-                 src="${largeImageURL}" alt="${tags}  loading="lazy" 
+                 src="${largeImageURL}" alt="${tags} loading="lazy" 
                 />
               </a>
             </div>
@@ -102,8 +101,13 @@ function createGalleryMarkup(images) {
     })
     .join('')
 
-  refs.gallery.innerHTML = markupGallery;
+  refs.gallery.insertAdjacentHTML('beforeend', markupGallery);
+  lightbox.refresh();
   refs.input.value = "";
 }
 
 
+function errorMessage() {
+  Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+  
+}
